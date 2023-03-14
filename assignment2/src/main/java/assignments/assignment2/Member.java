36e66af4711327e796a8c6c3d383b7fe2328cf57
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2023-03-14 11:18:13 +0700

    feat: finish Member.java

--- assignment2/src/main/java/assignments/assignment2/Member.java
+++ assignment2/src/main/java/assignments/assignment2/Member.java
@@ -4,9 +4,36 @@ import assignments.assignment1.NotaGenerator;
 
 public class Member {
     // TODO: tambahkan attributes yang diperlukan untuk class ini
+    private String nama;
+    private String noHp;
+    private String id;
+    private int bonusCounter;
+    
     public Member(String nama, String noHp) {
         // TODO: buat constructor untuk class ini
+        this.nama = nama;
+        this.noHp = noHp;
+        this.id = NotaGenerator.generateId(this.nama, this.noHp);
     }
 
     // TODO: tambahkan methods yang diperlukan untuk class ini
+    public String getId() {
+        return this.id;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public String getNoHp() {
+        return this.noHp;
+    }
+
+    public void incrementBonusCounter() {
+        this.bonusCounter++;
+    }
+
+    public int getBonusCounter() {
+        return this.bonusCounter;
+    }
 }
